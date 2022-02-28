ca110253cc8a2a3e9d9e7444c191a98e64ea8db8
Angela Candela <angelacandela@hotmail.it>
2022-02-28 16:59:31 +0100

    Create BaseLayersFactory

---
+++ src/components/Map/components/BaseLayersFactory.js
@@ -0,0 +1,22 @@
+import { useCallback } from 'react';
+import BaseLayer from './BaseLayer';
+
+import baselayers from '../../../data/baselayers.json';
+
+const BaseLayersFactory = () => {
+
+  const baselayersMaker = useCallback(
+    () => baselayers.map((baselayer, index) =>
+      <BaseLayer
+        key={index}
+        status={baselayer.default}
+        label={baselayer.label}
+        url={baselayer.url}
+      />
+    ), []
+  );
+
+  return baselayersMaker();
+};
+
+export default BaseLayersFactory;
